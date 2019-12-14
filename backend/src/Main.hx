b749f6817d8fbb8fa6051b68a2ebe10943b89d6e
Edouard Lopez <contact@edouard-lopez.com>
2019-12-15 00:32:33 +0100

    stat all existing code (explicity)

--- backend/src/Main.hx
+++ backend/src/Main.hx
@@ -9,11 +9,23 @@ class Main {
 		switch (Web.getURI()) {
 			case "/":
 			case "/stats":
-				var xx1Stats = new Stats(answers, 'XX1');
-				var output = {
-					code: xx1Stats.code,
-					stat: xx1Stats.compute()
-				};
+				var xx1 = new Stats(answers, 'XX1');
+				var xx2 = new Stats(answers, 'XX2');
+				var xx3 = new Stats(answers, 'XX3');
+				var output = [
+					{
+						code: xx1.code,
+						stat: xx1.compute()
+					},
+					{
+						code: xx2.code,
+						stat: xx2.compute()
+					},
+					{
+						code: xx3.code,
+						stat: xx3.compute()
+					}
+				];
 				Web.setReturnCode(200);
 				Web.setHeader('Content-Type', 'application/json');
 				Sys.print(Json.stringify(output));
