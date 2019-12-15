861f65dab6c9256538836f68cfd243efad5a976b
Edouard Lopez <contact@edouard-lopez.com>
2019-12-15 22:41:59 +0100

    convert <StatsGraph> to a hook function

--- frontend/src/StatsGraph.jsx
+++ frontend/src/StatsGraph.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import {
   Bar,
   BarChart,
@@ -10,32 +10,24 @@ import {
   YAxis
 } from "recharts";
 
-class StatsGraph extends Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    const { data } = this.props;
-
-    return (
-      <ResponsiveContainer width="100%" height={300}>
-        <BarChart
-          data={data.qcm}
-          margin={{ top: 5, right: 20, bottom: 5, left: 0 }}
-        >
-          <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
-          <CartesianGrid stroke="#f5f5f5" />
-          <XAxis dataKey="name" />
-          <YAxis />
-          <Tooltip />
-          <Legend />
-          <Bar dataKey="count" barSize={20} fill="#82ca9d" />
-          {/* <Line type="monotone" dataKey="predicted" stroke="#8884d8" /> */}
-        </BarChart>
-      </ResponsiveContainer>
-    );
-  }
+function StatsGraph({ data }) {
+  return (
+    <ResponsiveContainer width="100%" height={300}>
+      <BarChart
+        data={data.qcm}
+        margin={{ top: 5, right: 20, bottom: 5, left: 0 }}
+      >
+        <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+        <CartesianGrid stroke="#f5f5f5" />
+        <XAxis dataKey="name" />
+        <YAxis />
+        <Tooltip />
+        <Legend />
+        <Bar dataKey="count" barSize={20} fill="#82ca9d" />
+        {/* <Line type="monotone" dataKey="predicted" stroke="#8884d8" /> */}
+      </BarChart>
+    </ResponsiveContainer>
+  );
 }
 
 export default StatsGraph;
